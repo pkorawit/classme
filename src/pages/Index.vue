177eb9d2c8b75b7656b30d227c1f7944f92b1263
Worrapong <38399765+worrapong08@users.noreply.github.com>
2020-09-02 16:11:55 +0700

    API get data / edit submit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,143 +1,121 @@
 <template>
   <q-page class="bg-secondary">
-    <q-form @submit="onSubmit" class="q-gutter-md">
-      <q-dialog v-model="submitEmpty">
-        <q-card style="width: 300px">
-          <q-card-section>
-            <div class="text-h6">Notification</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            Please choose the answer.
-          </q-card-section>
-
-          <q-card-actions align="right" class="bg-white text-teal">
-            <q-btn flat label="OK" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-      <div class="row q-pa-sm">
-        <div class="col-3">
-          <q-input standout v-model="text_ID" readonly />
-        </div>
-        <div class="col-6"></div>
-        <div class="col-3">
-          <q-input
-            v-model="search_ID"
-            standout
-            type="search"
-            label="Search by ID"
-          >
-            <template v-slot:append>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-        </div>
+    <div class="row q-pa-sm">
+      <div class="col-3">
+        <q-input standout v-model="text_ID" readonly />
       </div>
-      <div class="row justify-center items-center">
-        <div class="col-2 text-center">
-          <q-btn
-            push
-            color="negative"
-            label="<< Previous"
-            type="submit"
-            @click="submitEmpty = true"
-          />
-        </div>
-        <div class="col-8 text-center">
-          <q-img
-            src="https://cdn.quasar.dev/img/parallax2.jpg"
-            style="height: 480px; max-width: 480px"
-            native-context-menu
-          >
-            <q-icon
-              class="absolute all-pointer-events"
-              size="32px"
-              name="info"
-              color="white"
-              style="top: 8px; left: 8px"
-            >
-              <q-tooltip>Tooltip</q-tooltip>
-            </q-icon>
-          </q-img>
-        </div>
-        <div class="col-2 text-center">
-          <q-btn
-            color="negative"
-            label="Next >>"
-            type="submit"
-            @click="submitEmpty = true"
-          />
-        </div>
+      <div class="col-6"></div>
+      <div class="col-3">
+        <q-input
+          v-model="search_ID"
+          standout
+          type="search"
+          label="Search by ID"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
       </div>
-      <div class="row q-mt-sm">
-        <div class="col-2"></div>
-        <div class="col-8 bg-info  items-start">
-          <div class="row">
-            <div class="col">
-              <q-input
-                filled
-                v-model="location_name"
-                readonly
-                label="Location Name"
-                stack-label
-              />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <q-input
-                filled
-                v-model="caption_text"
-                readonly
-                type="textarea"
-                label="Caption Text"
-                stack-label
-              />
-            </div>
+    </div>
+    <div class="row justify-center items-center">
+      <div class="col-2 text-center">
+        <q-btn
+          push
+          color="negative"
+          label="<< Previous"
+          type="submit"
+          @click="onPrevious"
+        />
+      </div>
+      <div class="col-8 text-center">
+        <q-img
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
+          style="height: 480px; max-width: 480px"
+          native-context-menu
+        >
+          <q-icon
+            class="absolute all-pointer-events"
+            size="32px"
+            name="info"
+            color="white"
+            style="top: 8px; left: 8px"
+          >
+            <q-tooltip>Tooltip</q-tooltip>
+          </q-icon>
+        </q-img>
+      </div>
+      <div class="col-2 text-center">
+        <q-btn color="negative" label="Next >>" type="submit" @click="onNext" />
+      </div>
+    </div>
+    <div class="row q-mt-sm">
+      <div class="col-2"></div>
+      <div class="col-8 bg-info  items-start">
+        <div class="row">
+          <div class="col">
+            <q-input
+              filled
+              v-model="location_name"
+              readonly
+              label="Location Name"
+              stack-label
+            />
           </div>
         </div>
-        <div class="col-2"></div>
-      </div>
-      <div class="row"></div>
-      <div class="row q-mt-sm">
-        <div class="col-2"></div>
-        <div class="col-8 bg-info  items-start">
-          <q-btn-toggle
-            name="toggle_advertisement"
-            v-model="toggle_advertisement"
-            spread
-            no-caps
-            toggle-color="purple"
-            color="white"
-            text-color="black"
-            :options="[
-              { label: 'Advertisement', value: 'T' },
-              { label: 'Not Advertisement', value: 'F' }
-            ]"
-          />
+        <div class="row">
+          <div class="col">
+            <q-input
+              filled
+              v-model="caption_text"
+              readonly
+              type="textarea"
+              label="Caption Text"
+              stack-label
+            />
+          </div>
         </div>
       </div>
-      <div class="row q-mt-sm">
-        <div class="col-2"></div>
-        <div class="col-8 bg-info  items-start">
-          <q-btn-toggle
-            name="toggle_tourism"
-            v-model="toggle_tourism"
-            spread
-            no-caps
-            toggle-color="pink"
-            color="white"
-            text-color="black"
-            :options="[
-              { label: 'Tourism', value: 'T' },
-              { label: 'Not Tourism', value: 'F' }
-            ]"
-          />
-        </div>
+      <div class="col-2"></div>
+    </div>
+    <div class="row"></div>
+    <div class="row q-mt-sm">
+      <div class="col-2"></div>
+      <div class="col-8 bg-info  items-start">
+        <q-btn-toggle
+          name="toggle_advertisement"
+          v-model="toggle_advertisement"
+          spread
+          no-caps
+          toggle-color="purple"
+          color="white"
+          text-color="black"
+          :options="[
+            { label: 'Advertisement', value: 'true' },
+            { label: 'Not Advertisement', value: 'false' }
+          ]"
+        />
       </div>
-      <div class="row q-mt-sm"></div>
-    </q-form>
+    </div>
+    <div class="row q-mt-sm">
+      <div class="col-2"></div>
+      <div class="col-8 bg-info  items-start">
+        <q-btn-toggle
+          name="toggle_tourism"
+          v-model="toggle_tourism"
+          spread
+          no-caps
+          toggle-color="pink"
+          color="white"
+          text-color="black"
+          :options="[
+            { label: 'Tourism', value: 'true' },
+            { label: 'Not Tourism', value: 'false' }
+          ]"
+        />
+      </div>
+    </div>
+    <div class="row q-mt-sm"></div>
   </q-page>
 </template>
 
@@ -146,6 +124,7 @@ export default {
   name: "PageIndex",
   data() {
     return {
+      id: 1,
       shortCode: null,
       text_ID: null,
       search_ID: null,
@@ -153,14 +132,15 @@ export default {
       caption_text: null,
       toggle_advertisement: null,
       toggle_tourism: null,
-      submitResult: [],
-      submitEmpty: false
     };
   },
+  mounted() {
+    this.init();
+  },
   methods: {
-    onSubmit(evt) {
+    init() {
       this.$axios
-        .get("https://insightapi-myzemjarqq-as.a.run.app/api/Posts/1")
+        .get("https://insightapi-myzemjarqq-as.a.run.app/api/Posts/" + this.id)
         .then(response => {
           // console.log(response.data);
           this.shortCode = response.data.shortCode;
@@ -168,38 +148,46 @@ export default {
           this.location_name = response.data.locationName;
           this.caption_text = response.data.captionText;
         });
-
-      const formData = new FormData(evt.target);
-      const submitResult = [];
-
-      for (const [name, value] of formData.entries()) {
-        submitResult.push({
-          name,
-          value
-        });
+    },
+    onPrevious() {
+      console.log("onPrevious");
+      if (this.toggle_advertisement == null && this.toggle_tourism == null) {
+        console.log("Do nothing");
+      }else{
+      this.onData();
+      this.id = this.id - 1;
+      if (this.id <= 0) {
+        this.id = 1;
       }
-      this.submitResult = submitResult;
-      this.submitEmpty = submitResult.length === 0;
-      // console.log("SubmitEmpty : " + this.submitEmpty);
-      if (this.submitEmpty == false) {
-        console.log("Add data in database");
-        // console.log(this.submitResult);
+      this.init();
+      }
+    },
+    onNext() {
+      console.log("onNext");
+      if (this.toggle_advertisement == null && this.toggle_tourism == null) {
+        console.log("Do nothing");
+      } else {
+      this.onData();
+      this.id = this.id + 1;
+      this.init();
+      }
+    },
+
+    onData() {
+        this.dialog == true;
         if (this.toggle_advertisement == null) {
-          this.toggle_advertisement = "F";
+          this.toggle_advertisement = "false";
         }
         if (this.toggle_tourism == null) {
-          this.toggle_tourism = "F";
+          this.toggle_tourism = "false";
         }
-        if (this.toggle_tourism == "T") {
-          this.toggle_advertisement = "F";
+        if (this.toggle_tourism == "true") {
+          this.toggle_advertisement = "false";
         }
         console.log("shortCode : " + this.shortCode);
         console.log("advertisement : " + this.toggle_advertisement);
         console.log("tourism : " + this.toggle_tourism);
-      } else {
-        console.log("Do nothing");
       }
-    }
   }
 };
 </script>
